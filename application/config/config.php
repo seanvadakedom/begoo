e350662712dfa5df53dbbe7d1804ea134a131108
hafeez1042 <hafeez1042@gmil.com>
2015-05-13 23:49:12 +0530

    first commit

--- application/config/config.php
+++ application/config/config.php
@@ -46,7 +46,7 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  * Configuration for: Database
  * This is the place where you define your database credentials, database type etc.
  */
-define('DB_TYPE', 'mysql');
+define('DB_TYPE', 'postgres');
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'begoo');
 define('DB_USER', 'root');
